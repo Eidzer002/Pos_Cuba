a97d6817321e1bf50dad92abd655d386bc6f1c1e
Eidzer002 <166095140+Eidzer002@users.noreply.github.com>
2026-05-31 14:35:32 -0400

    add lib/providers/providers.dart

---
+++ lib/providers/providers.dart
@@ -0,0 +1,13 @@
+// lib/providers/providers.dart
+// Exporta todos los providers.
+
+export 'auth_provider.dart';
+export 'business_provider.dart';
+export 'category_provider.dart';
+export 'product_provider.dart';
+export 'cart_provider.dart';
+export 'sale_provider.dart';
+export 'cashbox_provider.dart';
+export 'worker_provider.dart';
+export 'worker_session_provider.dart';
+export 'report_provider.dart';
