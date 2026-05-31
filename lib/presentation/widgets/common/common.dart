b7051221ecee0baf419221db7c39bd90e2d45e07
Eidzer002 <166095140+Eidzer002@users.noreply.github.com>
2026-05-31 14:35:21 -0400

    add lib/presentation/widgets/common/common.dart

---
+++ lib/presentation/widgets/common/common.dart
@@ -0,0 +1,9 @@
+// lib/presentation/widgets/common/common.dart
+// Exporta widgets comunes.
+
+export 'loading_indicator.dart';
+export 'error_widget.dart';
+export 'empty_state.dart';
+export 'currency_text.dart';
+export 'app_scaffold.dart';
+export 'product_card.dart';
