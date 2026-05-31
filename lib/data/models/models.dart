272c4e2a11062887ef64670856257feac60f706c
Eidzer002 <166095140+Eidzer002@users.noreply.github.com>
2026-05-31 14:34:54 -0400

    add lib/data/models/models.dart

---
+++ lib/data/models/models.dart
@@ -0,0 +1,12 @@
+// lib/data/models/models.dart
+// Exporta todos los modelos de datos.
+
+export 'business.dart';
+export 'category.dart';
+export 'product.dart';
+export 'sale.dart';
+export 'sale_item.dart';
+export 'stock_movement.dart';
+export 'cash_session.dart';
+export 'cash_movement.dart';
+export 'worker.dart';
