b2aeef1fdb8db2c4f8c4e7c6bc81517739a5d65b
Eidzer002 <166095140+Eidzer002@users.noreply.github.com>
2026-05-31 14:35:06 -0400

    add lib/data/repositories/repositories.dart

---
+++ lib/data/repositories/repositories.dart
@@ -0,0 +1,9 @@
+// lib/data/repositories/repositories.dart
+// Exporta todos los repositorios.
+
+export 'product_repository.dart';
+export 'sale_repository.dart';
+export 'category_repository.dart';
+export 'cash_session_repository.dart';
+export 'worker_repository.dart';
+export 'report_repository.dart';
